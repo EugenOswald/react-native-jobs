bff82caf1ad58fcae06f0536b4c8fd3b6b00d62c
Eugen Oswald <eugen.oswald@kemweb.de>
2023-11-15 17:54:21 +0100

    update 43:25

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+	const router = useRouter();
+	return (
+		<View>
+			<View style={styles.container}>
+				<Text style={styles.userName}>Hallo Eugen</Text>
+				<Text style={styles.welcomeMessage}>Find your perfect job</Text>
+			</View>
 
-export default Welcome
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput style={styles.searchInput} value='' onChange={() => {}} placeholder='What are you looking for?' />
+				</View>
+			</View>
+
+			<TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+				<Image source={icons.search} resizeMode='contain' style={styles.searchBtnImage} />
+			</TouchableOpacity>
+
+			<View style={styles.tabsContainer}>
+				<FlatList />
+			</View>
+		</View>
+	);
+};
+
+export default Welcome;
