e6046d4096ee4754f3c48a62fcdefa11f1dbf80f
Eugen Oswald <eugen.oswald@kemweb.de>
2023-11-15 16:06:20 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+import { Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+const Home = () => {
+	return (
+		<View>
+			<Text>Home</Text>
+		</View>
+	);
+};
+
+export default Home;
